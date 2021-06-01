cd872b7c5e87b421c680bdbb00dca645977928a4
tiennv0608 <tiennv0608@gmail.com>
2021-06-01 14:55:33 +0700

    b4

--- 04_B4_TaoThreaddongian/src/NumberGenerator.java
+++ 04_B4_TaoThreaddongian/src/NumberGenerator.java
@@ -14,7 +14,7 @@ public class NumberGenerator implements Runnable {
     }
 
     @Override
-    public void run() {
+    public synchronized void run() {
         for (int i = 0; i < 10; i++) {
             System.out.println(threadName + ": " + i);
             try {
